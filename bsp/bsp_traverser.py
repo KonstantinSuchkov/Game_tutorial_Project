82206ddaa0e6a7261dde02ea8c39440f1066a9e4
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2024-03-17 15:35:43 +0300

    Initial commit - setup, BSP tree

---
+++ bsp/bsp_traverser.py
@@ -0,0 +1,37 @@
+from settings import *
+from data_types import BSPNode
+from utils import is_on_front
+
+
+class BSPTreeTraverser:
+    def __init__(self, engine):
+        self.engine = engine
+        self.root_node = engine.bsp_builder.root_node
+        self.segments = engine.bsp_builder.segments
+
+        #temp stuff
+        self.cam_pos = vec2(6, 7)
+        self.seg_ids_to_draw = []
+
+    def update(self):
+        self.seg_ids_to_draw.clear()
+        self.traverse(self.root_node)
+
+    def traverse(self, node: BSPNode):
+        if node is None:
+            return None
+
+        on_front = is_on_front(self.cam_pos - node.splitter_p0, node.splitter_vec)
+        #
+        if on_front:
+            self.traverse(node.front)
+            #
+            self.seg_ids_to_draw.append(node.segment_id)
+            #
+            self.traverse(node.back)
+        else:
+            self.traverse(node.back)
+            #
+            self.seg_ids_to_draw.append(node.segment_id)
+            #
+            self.traverse(node.front)
---
+++ engine.py
@@ -0,0 +1,33 @@
+from settings import *
+from level_data import LevelData
+from map_renderer import MapRenderer
+from bsp.bsp_builder import BSPTreeBuilder
+from bsp.bsp_traverser import BSPTreeTraverser
+
+
+class Engine:
+    def __init__(self, app):
+        self.app = app
+        #
+        self.level_data = LevelData(self)
+        self.bsp_builder = BSPTreeBuilder(self)
+        self.bsp_traverser = BSPTreeTraverser(self)
+        self.map_renderer = MapRenderer(self)
+
+    def update(self):
+        self.bsp_traverser.update()
+
+    def draw_2d(self):
+        self.map_renderer.draw()
+
+    def draw_3d(self):
+        pass
+
+    def draw(self):
+        ray.begin_drawing()
+        #
+        ray.clear_background(ray.BLACK)
+        self.draw_3d()
+        self.draw_2d()
+        #
+        ray.end_drawing()
